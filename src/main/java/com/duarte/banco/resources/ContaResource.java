997e8f26956f0f287e39c889cd0143f196729e5a
Antonio Duarte <antoniord@outlook.com>
2018-09-03 16:17:36 -0300

    Criado repository e service  para contas

--- src/main/java/com/duarte/banco/resources/ContaResource.java
+++ src/main/java/com/duarte/banco/resources/ContaResource.java
@@ -1,30 +1,26 @@
 package com.duarte.banco.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.duarte.banco.domain.Conta;
+import com.duarte.banco.service.ContaService;
 
 @RestController
 @RequestMapping(value = "/contas")
 public class ContaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Conta> listar() {
-
-		Conta c1 = new Conta(1, 1000, "Duarte", 2000);
-		Conta c2 = new Conta(2, 2000, "Vilma", 2000);
-
-		List<Conta> lista = new ArrayList<Conta>();
-
-		lista.add(c1);
-		lista.add(c2);
-
-		return lista;
+	@Autowired
+	private ContaService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Conta obj = service.find(id);
+		return ResponseEntity.ok(obj);
 	}
 
 }
